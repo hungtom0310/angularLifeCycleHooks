7e12e78aa442743851fd0d93442c51f938eca289
hungtom0310 <59992121+hungtom0310@users.noreply.github.com>
2022-09-09 22:46:20 +0700

    a

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
 })
-export class ChildComponent implements OnInit, OnDestroy {
+export class ChildComponent implements OnInit, OnDestroy, OnChanges {
+  @Input()
   counter = 0;
   interval: any;
+  public channelName = '';
   constructor() {
     console.log('1.Child Contructor is called.');
   }
@@ -15,13 +17,17 @@ export class ChildComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     console.log('1.Child OnInit is called.');
 
-    this.interval = setInterval(() => {
-      this.counter = this.counter + 1;
-      console.log(this.counter);
-    }, 1000);
+    // this.interval = setInterval(() => {
+    //   this.counter = this.counter + 1;
+    //   console.log(this.counter);
+    // }, 1000);
   }
   ngOnDestroy(): void {
-    clearInterval(this.interval);
+    // clearInterval(this.interval);
     console.log('2.Child OnDestroy is called.');
+  }
+
+  ngOnChanges(): void {
+    console.log('3.Child OnChanges is called.');
   }
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
-export class ParentComponent implements OnInit {
-  isChild = false;
+export class ParentComponent implements OnInit, OnChanges {
+  isChild = true;
   constructor() {
     console.log('1.Parent Contructor is called.');
   }
 
   ngOnInit(): void {
     console.log('1.Parent OnInit is called.');
+  }
+
+  ngOnChanges(): void {
+    console.log('3.Parent OnChanges is called.');
   }
   toggleChild() {
     this.isChild = !this.isChild;
