af91ef20644b15702c98b1dd5097164db73f322b
hungtom0310 <59992121+hungtom0310@users.noreply.github.com>
2022-09-09 21:58:13 +0700

    á

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -6,12 +6,18 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent implements OnInit, OnDestroy {
+  counter = 0;
   constructor() {
     console.log('1.Child Contructor is called.');
   }
 
   ngOnInit(): void {
     console.log('1.Child OnInit is called.');
+
+    setInterval(() => {
+      this.counter = this.counter + 1;
+      console.log(this.counter);
+    }, 1000);
   }
   ngOnDestroy(): void {
     console.log('2.Child OnDestroy is called.');
