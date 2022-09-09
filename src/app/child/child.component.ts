92628344d8c8932d631443f5d27840629c13b45c
hungtom0310 <59992121+hungtom0310@users.noreply.github.com>
2022-09-09 21:47:22 +0700

    a

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnDestroy {
   constructor() {
     console.log('1.Child Contructor is called.');
   }
 
   ngOnInit(): void {
     console.log('1.Child OnInit is called.');
+  }
+  ngOnDestroy(): void {
+    console.log('2.Child OnDestroy is called.');
   }
 }
