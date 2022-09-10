d25e178dbe250db0d33847f7de75425a0d51e982
hungtom0310 <59992121+hungtom0310@users.noreply.github.com>
2022-09-10 10:37:26 +0700

    A

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,5 +1,7 @@
 import {
+  AfterContentInit,
   Component,
+  ContentChild,
   DoCheck,
   Input,
   OnChanges,
@@ -13,23 +15,24 @@ import {
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
 })
-export class ChildComponent implements OnInit, OnDestroy, OnChanges, DoCheck {
+export class ChildComponent
+  implements OnInit, OnDestroy, OnChanges, DoCheck, AfterContentInit
+{
   counter = 0;
   interval: any;
 
   @Input()
   channelName = '';
+
+  @ContentChild('projectedContent') protectedContent: any;
+
   constructor() {
     console.log('1.Child Contructor is called.');
   }
 
   ngOnInit(): void {
     console.log('1.Child OnInit is called.');
-
-    // this.interval = setInterval(() => {
-    //   this.counter = this.counter + 1;
-    //   console.log(this.counter);
-    // }, 1000);
+    console.log('6.OnInit - ' + this.protectedContent);
   }
   ngOnDestroy(): void {
     // clearInterval(this.interval);
@@ -38,9 +41,15 @@ export class ChildComponent implements OnInit, OnDestroy, OnChanges, DoCheck {
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log('3.Child OnChanges is called.', changes);
+    console.log('6.OnChanges - ' + this.protectedContent);
   }
 
   ngDoCheck(): void {
     console.log('4.Child DoCheck is called.');
+    console.log('6.DoCheck - ' + this.protectedContent);
+  }
+  ngAfterContentInit(): void {
+    console.log('5.In After Content Init');
+    console.log('6.After Content Init - ' + this.protectedContent);
   }
 }
